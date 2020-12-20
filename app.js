cdd9a606eaec41b0c15446a12622172fcbe550ec
Lexi <ehdubya@hotmail.com>
2020-12-20 14:29:00 -0800

    Create index, public and 404 routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { Pool } = require("pg");
 
 app = express();
+app.set("view engine", "ejs");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -13,6 +14,16 @@ const pool = new Pool({
 pool.connect((err) => {
   if (err) throw err;
   console.log("Database connection successfully established");
+});
+
+app.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("*", (req, res, next) => {
+  res.render("error", { code: "404", message: "Not Found" });
 });
 
 module.exports = app;
