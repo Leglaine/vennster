ff0ddc154c82d959a43dbee895c1a35e451fd85e
Lexi <ehdubya@hotmail.com>
2020-12-20 14:05:04 -0800

    Connect to database

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require("express");
+const { Pool } = require("pg");
 
 app = express();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+pool.connect((err) => {
+  if (err) throw err;
+  console.log("Database connection successfully established");
+});
 
 module.exports = app;
