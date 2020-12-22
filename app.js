df5fcf587f5f210e2d072cb2249f21d30fb997a7
Lexi <ehdubya@hotmail.com>
2020-12-21 23:38:39 -0800

    Render layout instead of views

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const { requireLogin } = require("./utils/login");
 
 app = express();
 app.set("view engine", "ejs");
+
+// Allow express to parse form input
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
@@ -16,25 +18,36 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.locals.user = req.session.user;
+  next();
+});
+
 app.use("/api/v1/users", usersRouter);
 
 app.get("/", requireLogin, (req, res, next) => {
-  res.render("index");
+  res.render("layout", { title: "Home", main: "index" });
 });
 
 app.get("/signup", (req, res, next) => {
-  res.render("signup");
+  res.render("layout", { title: "Sign Up", main: "signup" });
 });
 
 app.get("/login", (req, res, next) => {
   delete req.session.user;
-  res.render("login");
+  res.locals.user = null;
+  res.render("layout", { title: "Log In", main: "login" });
 });
 
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("*", (req, res, next) => {
-  res.render("error", { code: "404", message: "Not Found" });
+  res.render("layout", {
+    title: "Error",
+    main: "error",
+    code: "404",
+    message: "Not Found",
+  });
 });
 
 app.use((err, req, res, next) => {
