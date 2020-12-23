504590e8f867ac0a4c85e5bad3340aade91b48f5
Lexi <ehdubya@hotmail.com>
2020-12-22 22:26:23 -0800

    Add comment

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const app = require("./app");
 
 const protocol = process.env.PROTOCOL;
 const host = process.env.HOST;
+
+/*
+The port is set here instead of in the
+.env file in order to mimic the production
+environment as closely as possible, as
+Heroku sets it automatically.
+*/
+
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
