9f39d1de4f08c5c057f280ba4d8dcccc3c387bce
Lexi <ehdubya@hotmail.com>
2020-12-27 20:44:56 -0800

    Set profile image

--- api/edit-profile/editProfileController.js
+++ api/edit-profile/editProfileController.js
@@ -1,3 +1,14 @@
+const db = require("../../db");
+
 exports.getEditProfile = (req, res, next) => {
   res.render("layout", { title: "Edit Profile", main: "editProfile" });
+};
+
+exports.postEditProfile = (req, res, next) => {
+  const url = req.body["profile-image"];
+  db.query("UPDATE users SET profile_image = $1 WHERE id = $2", [
+    url,
+    req.session.user,
+  ]);
+  res.redirect("/edit-profile");
 };
--- api/edit-profile/editProfileRouter.js
+++ api/edit-profile/editProfileRouter.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const editProfileController = require("./editProfileController");
 
 router.get("/", editProfileController.getEditProfile);
+router.post("/", editProfileController.postEditProfile);
 
 module.exports = router;
