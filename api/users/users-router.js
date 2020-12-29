9fdf5acc1cb953a5bb43f02716297df1230f21e5
Lexi Wright <ehdubya@hotmail.com>
2020-12-29 00:43:57 -0800

    Require login

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const usersController = require("./users-controller");
+const { requireLogin } = require("../../utils/login");
 
-router.get("/:id", usersController.getUser);
+router.get("/:id", requireLogin, usersController.getUser);
 
 module.exports = router;
