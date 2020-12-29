57133dc0a1632db0fbdbe7c2ab1a2eeed22d64d7
Lexi Wright <ehdubya@hotmail.com>
2020-12-29 00:43:35 -0800

    Add bio

--- api/users/users-controller.js
+++ api/users/users-controller.js
@@ -1,7 +1,7 @@
 const db = require("../../db");
 const asyncHandler = require("../../utils/async");
 
-exports.getUser = asyncHandler(async (req, res, next) => {
+exports.getUser = asyncHandler(async (req, res, _next) => {
   const id = req.params.id;
 
   const response = await db.query("SELECT * FROM users WHERE id = $1", [id]);
@@ -9,11 +9,13 @@ exports.getUser = asyncHandler(async (req, res, next) => {
   const name = response["rows"][0]["name"];
   const profileImage = response["rows"][0]["profile_image"];
   const coverImage = response["rows"][0]["cover_image"];
+  const bio = response["rows"][0]["bio"];
 
   res.render("layout", {
     title: name,
     main: "profile",
     profileImage: profileImage,
     coverImage: coverImage,
+    bio: bio,
   });
 });
